7851c5fa5cced91806f1f121066a9948661d0bb6
sa46lll <9812smh@naver.com>
2021-12-30 09:20:08 +0900

    [Table_Component] VectorTabelModelEx table addHandler 추가

--- src/Table/VectorTableModelEx.java
+++ src/Table/VectorTableModelEx.java
@@ -1,3 +1,4 @@
+package Table;
 
 // 4. 사용자 표 모델 이용 - 벡터와 연
 import javax.swing.*;
@@ -15,7 +16,8 @@ public class VectorTableModelEx extends JFrame {
 	public VectorTableModelEx() {
 		super("테이블 테스트4");
 		
-		// mData = Person의 벡터 생;
+		// mData = Person의 벡터 생성;
+		mData = new Vector<Person>();
 		
 		buildGUI();
 		
@@ -28,6 +30,8 @@ public class VectorTableModelEx extends JFrame {
 	private void buildGUI() {
 		// 표
 		// mData와 연계된 사용자정의테이블모델 VectorTableModel을 기반으로 테이블 생성
+		mTable = new JTable();
+		mTable.setModel(new VectorTableModel(mData));
 		
 		// 버튼
 		JPanel p_button = new JPanel();
@@ -49,8 +53,16 @@ public class VectorTableModelEx extends JFrame {
 
 		@Override
 		public void actionPerformed(ActionEvent ae) {
+			
 			// 벡터에 데이터 추가
+			int num = mTable.getRowCount();
+			for (int i = num; i < num + 5; i++) {
+				Person p = new Person("홍길동" + i, 20 + i, "010=1234-" + (1000+i));
+				mData.add(p);
+			}
+			
 			// 테이블 갱신
+			mTable.updateUI();
 		}
 		
 	};
@@ -91,6 +103,47 @@ class VectorTableModel extends AbstractTableModel {
 
 	private Vector<String> mColumnNames;
 	private Vector<Person> mData;
+	
+	public VectorTableModel(Vector<Person> v) {
+		String[] s = {"이름", "나이", "전화번호"};
+		mColumnNames = new Vector<String>(s.length);
+		
+		for ( int c = 0; c < s.length; c++ )
+			mColumnNames.addElement(s[c]);
+		
+		//셀 데이터 설정
+		mData = v;
+	}
+	
+	@Override
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return mColumnNames.size();
+	}
+	@Override
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return mData.size();
+	}
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		Person p = mData.get(rowIndex);
+		
+		switch(columnIndex) {
+		case 0: return p.getName();
+		case 1: return p.getAge();
+		case 2: return p.getPhoneNum();
+		}
+		
+		return null;
+	}
+	@Override
+	public String getColumnName(int column) {
+		// TODO Auto-generated method stub
+		return mColumnNames.get(column);
+	}
+	
+	
 
 }
 
